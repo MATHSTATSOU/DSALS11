46265895abdcc5ef812f066b701e81a9b4e013fd
Wayne Stewart <wayne.s.stewart@ou.edu>
2024-10-29 10:25:04 -0500

    Run Jags script with function

---
+++ R/mypaired.R
@@ -0,0 +1,11 @@
+#' My Paired 2 Sample Problem
+#'
+#' @return Jags output
+#' @export
+#'
+#' @examples
+#' \dontrun{mypaired()}
+mypaired <- function(){
+  source(system.file("jags/LS11/Jags-PairedSampleScriptMV-wayne.R", package = "DSALS11"))
+}
+
